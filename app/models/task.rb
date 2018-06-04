f6027857eb61e1af2283aeb68988f6a6e944aa56
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-06-04 19:44:02 +0900

    create model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ApplicationRecord
+  validates :task_name, presence: true, length: {minimun: 1, maximum: 30}, uniqueness: true
+  validates :deadline, presence: true
+
+  scope :undone_status, -> {
+    where(status: false).order("created_at DESC")}
+
+  def self.search(search)
+    if search
+      Task.where(["task_name like ?", "%#{search}%"])
+    end
+  end
+
 end
